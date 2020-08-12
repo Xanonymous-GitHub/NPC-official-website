242905886e1cf9f67d2130056b8df342058753d1
Xanonymous <trusaidlin@gmail.com>
2020-08-12 12:06:01 +0800

    close unuse route and increase main title font-size.

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,51 +10,51 @@ const routes: Array<RouteRecordRaw> = [
       '@/pages/Home.vue'
       )
   },
-  {
-    path: '/dashboard',
-    name: 'DashBoard',
-    component: () => import(
-      /* webpackChunkName: "DashBoard" */
-      /* webpackPrefetch: true */
-      '@/pages/DashBoard.vue'
-      )
-  },
-  {
-    path: '/login',
-    name: 'Login',
-    component: () => import(
-      /* webpackChunkName: "Login" */
-      /* webpackPrefetch: true */
-      '@/pages/Login.vue'
-      )
-  },
-  {
-    path: '/registration',
-    name: 'Registration',
-    component: () => import(
-      /* webpackChunkName: "Registration" */
-      /* webpackPrefetch: true */
-      '@/pages/Registration.vue'
-      )
-  },
-  {
-    path: '/admin',
-    name: 'Admin',
-    component: () => import(
-      /* webpackChunkName: "Admin" */
-      /* webpackPrefetch: true */
-      '@/pages/Admin.vue'
-      )
-  },
-  {
-    path: '/fixing',
-    name: 'Fixing',
-    component: () => import(
-      /* webpackChunkName: "Fixing" */
-      /* webpackPrefetch: true */
-      '@/pages/Fixing.vue'
-      )
-  },
+  // {
+  //   path: '/dashboard',
+  //   name: 'DashBoard',
+  //   component: () => import(
+  //     /* webpackChunkName: "DashBoard" */
+  //     /* webpackPrefetch: true */
+  //     '@/pages/DashBoard.vue'
+  //     )
+  // },
+  // {
+  //   path: '/login',
+  //   name: 'Login',
+  //   component: () => import(
+  //     /* webpackChunkName: "Login" */
+  //     /* webpackPrefetch: true */
+  //     '@/pages/Login.vue'
+  //     )
+  // },
+  // {
+  //   path: '/registration',
+  //   name: 'Registration',
+  //   component: () => import(
+  //     /* webpackChunkName: "Registration" */
+  //     /* webpackPrefetch: true */
+  //     '@/pages/Registration.vue'
+  //     )
+  // },
+  // {
+  //   path: '/admin',
+  //   name: 'Admin',
+  //   component: () => import(
+  //     /* webpackChunkName: "Admin" */
+  //     /* webpackPrefetch: true */
+  //     '@/pages/Admin.vue'
+  //     )
+  // },
+  // {
+  //   path: '/fixing',
+  //   name: 'Fixing',
+  //   component: () => import(
+  //     /* webpackChunkName: "Fixing" */
+  //     /* webpackPrefetch: true */
+  //     '@/pages/Fixing.vue'
+  //     )
+  // },
 ]
 
 const router = createRouter({
