093135025270dcbb88a6751de0f824678d495102
Xanonymous <trusaidlin@gmail.com>
2020-08-14 00:55:17 +0800

    add wavy css

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,9 +2,18 @@
   <main class="page-container" id="home">
     <div class="first-area">
       <div class="clip-area">
-        <div id="main-logo-container">
-          <MainLogo/>
-        </div>
+        <svg class="waves" preserveAspectRatio="none" shape-rendering="auto"
+             viewBox="0 24 150 28" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+          <defs>
+            <path d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" id="gentle-wave"/>
+          </defs>
+          <g class="parallax">
+            <use fill="rgba(255,255,255,0.7" x="48" xlink:href="#gentle-wave" y="0"/>
+            <use fill="rgba(255,255,255,0.5)" x="48" xlink:href="#gentle-wave" y="3"/>
+            <use fill="rgba(255,255,255,0.3)" x="48" xlink:href="#gentle-wave" y="5"/>
+            <use fill="#fff" x="48" xlink:href="#gentle-wave" y="7"/>
+          </g>
+        </svg>
       </div>
       <div class="main-title">
         {{ mainTitle.line1 }}
@@ -24,7 +33,7 @@
 
 <script lang="ts">
 import {defineComponent, onMounted} from 'vue';
-import MainLogo from "@/components/home/MainLogo.vue";
+// import MainLogo from "@/components/home/MainLogo.vue";
 import '@/assets/images/programmer.svg';
 import '@/assets/scss/pages/home.scss'
 
@@ -32,7 +41,7 @@ export default defineComponent({
   name: "Home",
   props: {},
   components: {
-    MainLogo
+    // MainLogo
   },
   data() {
     return {
