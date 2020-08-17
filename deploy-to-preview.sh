9362ed53775ae5b4224ddb340bedbe8360b95e81
Xanonymous <trusaidlin@gmail.com>
2020-08-17 23:26:13 +0800

    add auto preview shell script

--- deploy-to-preview.sh
+++ deploy-to-preview.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 git clone git@github.com:Xanonymous-GitHub/NPC-official-website-Preview.git ../NPC-official-website-Preview
 rm -rf ../NPC-official-website-Preview/*
-rsync -vrh --exclude 'node_modules' --exclude 'dist' --exclude 'LICENSE' --exclude '*.md' ./* ../NPC-official-website-Preview/
+rsync -vrh --exclude 'node_modules' --exclude 'dist' --exclude 'LICENSE' --exclude '*.md' --exclude '*.sh' ./* ../NPC-official-website-Preview/
 cd ../NPC-official-website-Preview || exit
 git add . && git commit -m "snap-shot-$(date +"%s")" && git push -f
 cd ..
