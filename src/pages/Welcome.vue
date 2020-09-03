497de5d45ce4f9381229b46961651c44a3166978
Xanonymous <trusaidlin@gmail.com>
2020-09-04 03:54:25 +0800

    finished page

--- src/pages/Welcome.vue
+++ src/pages/Welcome.vue
@@ -1,13 +1,43 @@
 <template>
   <main class="page-container" id="welcome">
-    welcome
+    <div class="contain contain__default-bg">
+      <div class="congrats">
+        <h1 v-if="!showCongratulation">{{ displayMessages.title }}</h1>
+        <h1 v-if="showCongratulation">Congrat<span class="hide">ulation</span>s !</h1>
+        <div class="done">
+          <div class="dot-spin" v-if="showProgressbar"/>
+          <svg v-if="!showCongratulation" viewBox="0 0 1 1">
+            <use :xlink:href="displayMessages.icon"/>
+          </svg>
+          <svg id="tick" style="enable-background:new 0 0 37 37;" version="1.1" viewBox="0 0 37 37"
+               x="0px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg"
+               xmlns:xlink="http://www.w3.org/1999/xlink" y="0px">
+          <path class="circle path"
+                d="M30.5,6.5L30.5,6.5c6.6,6.6,6.6,17.4,0,24l0,0c-6.6,6.6-17.4,6.6-24,0l0,0c-6.6-6.6-6.6-17.4,0-24l0,0C13.1-0.2,23.9-0.2,30.5,6.5z"
+                style="fill:#0cdcc7;stroke:#07a796;stroke-width:3;stroke-linejoin:round;stroke-miterlimit:10;"/>
+            <polyline class="tick path" points="11.6,20 15.9,24.2 26.4,13.8 "
+                      style="fill:none;stroke:#fff;stroke-width:3;stroke-linejoin:round;stroke-miterlimit:10;"/>
+          </svg>
+        </div>
+        <div class="text">
+          <p :key="key" v-for="(msg, key) in displayMessages.content">
+            {{ msg }}
+          </p>
+          <p class="rank" v-if="showRank">你的名次：{{ rank }}
+            <br/><span style="font-size: 0.2rem">{{ verificationCode }}</span></p>
+        </div>
+        <p class="regards">NPC 北科程式設計研究社</p>
+      </div>
+    </div>
   </main>
   <slot name="footer"></slot>
 </template>
 
 <script lang="ts">
-import {defineComponent, inject, onMounted} from 'vue';
+import {defineComponent, inject, onMounted, reactive, toRefs} from 'vue';
 import '@/assets/scss/pages/welcome.scss'
+import '@/assets/images/kiss-regular.svg'
+import '@/assets/images/exclamation-circle-solid.svg'
 import {
   firebaseType,
   firebaseDocumentType,
@@ -19,7 +49,17 @@ import {
   getFirebaseDocuments
 } from "@/utils/firebase";
 
-type freshmenPartyDocumentType = { remaining_awards: number, winners: Array<{ timestamp: firebaseTimeStampType, anonymous_uid: string }> }
+type winnerType = {
+  timestamp: firebaseTimeStampType,
+  anonymous_uid: string,
+  winner_index: number
+}
+
+type freshmenPartyDocumentType = {
+  remaining_awards: number,
+  winner_index: number,
+  winners: Array<winnerType>
+}
 
 export default defineComponent({
   name: "Welcome",
@@ -35,24 +75,30 @@ export default defineComponent({
     class eligibilityManager {
       public isAlreadyWinner!: boolean
       public registryUidEligibility!: boolean
+      public rank!: number
+      public userUid!: string
 
       constructor(freshmenPartyDocumentData: freshmenPartyDocumentType) {
         const currentUser = getCurrentUser(firebase)
-        const {remaining_awards, winners} = freshmenPartyDocumentData
+        this.userUid = (currentUser?.uid) as string
+        const {remaining_awards, winners, winner_index} = freshmenPartyDocumentData
         this.isAlreadyWinner = winners.map(winner => winner.anonymous_uid).includes(((currentUser as firebaseUserType)?.uid) as string)
         this.registryUidEligibility = remaining_awards > 0 && !this.isAlreadyWinner
+        this.rank = this.isAlreadyWinner ? (winners.find(winner => winner.anonymous_uid === ((currentUser as firebaseUserType)?.uid) as string) as winnerType).winner_index : winner_index
       }
     }
 
     const registryWinnerDevice = async (freshmenPartyDocuments: firebaseDocumentType, freshmenPartyDocumentData: freshmenPartyDocumentType): Promise<void> => {
-      const {remaining_awards, winners} = freshmenPartyDocumentData
+      const {remaining_awards, winners, winner_index} = freshmenPartyDocumentData
       winners.push({
         anonymous_uid: (firebase.auth().currentUser?.uid as string),
-        timestamp: (getTimeStamp() as firebaseTimeStampType)
+        timestamp: (getTimeStamp() as firebaseTimeStampType),
+        winner_index
       })
       await freshmenPartyDocuments.update({
         remaining_awards: remaining_awards > 0 ? remaining_awards - 1 : 0,
-        winners
+        winners,
+        winner_index: winner_index + 1
       })
     }
 
@@ -62,7 +108,8 @@ export default defineComponent({
         const freshmenPartyDocuments = await getFirebaseDocuments('/public/events/freshmen_party/', 'data')
         const freshmenPartyDocumentData = ((await freshmenPartyDocuments.get()).data()) as freshmenPartyDocumentType
         const anonymousUserEligibility = new eligibilityManager(freshmenPartyDocumentData)
-        console.log(anonymousUserEligibility)
+        data.rank = anonymousUserEligibility.rank
+        data.verificationCode = anonymousUserEligibility.userUid
         if (anonymousUserEligibility.registryUidEligibility) {
           await registryWinnerDevice(freshmenPartyDocuments, freshmenPartyDocumentData)
           return gameResults.WINNER
@@ -73,11 +120,79 @@ export default defineComponent({
       }
     }
 
+    const displayMessages = {
+      win: {
+        title: '',
+        content: [
+          '太神啦！',
+          '你們是超快過關的組別！',
+          '快點去找工作人員拿特殊獎勵吧！'
+        ]
+      },
+      loss: {
+        title: 'oops!',
+        content: [
+          '太可惜了...',
+          '雖然成功破關了，但是特殊獎勵已經被其他組領走了哦!',
+          '但我們還有準備安慰獎，去找工作人員領取吧！'
+        ],
+        icon: '#kiss-regular.svg'
+      },
+      connecting: {
+        title: 'Connecting...',
+        content: [
+          '判定獎項中',
+          '正在連接至遊戲伺服器'
+        ]
+      },
+      error: {
+        title: 'Oh No!',
+        content: [
+          '連線至遊戲伺服器時發生問題！',
+          '請重新整理頁面或洽詢工作人員！'
+        ],
+        icon: '#exclamation-circle-solid.svg'
+      }
+    }
+
+    const data = reactive({
+      showProgressbar: true,
+      showCongratulation: false,
+      displayMessages: displayMessages.connecting,
+      rank: 0,
+      showRank: false,
+      verificationCode: ''
+    })
+
     onMounted(async () => {
       const gameResult = await getFreshmenPartyGameResult()
-      console.log(gameResult)
+      setTimeout(() => {
+        data.showProgressbar = false;
+        switch (gameResult) {
+          case gameResults.WINNER:
+            data.showCongratulation = true;
+            (document.querySelector('.contain') as HTMLDivElement).classList.remove('contain__default-bg');
+            (document.querySelector('.contain') as HTMLDivElement).classList.add('contain__winner-bg')
+            data.displayMessages = displayMessages.win;
+            (document.querySelector('.done') as HTMLDivElement).classList.add('drawn')
+            data.showRank = true;
+            break;
+          case gameResults.LOSER:
+            (document.querySelector('.contain') as HTMLDivElement).classList.remove('contain__default-bg');
+            (document.querySelector('.contain') as HTMLDivElement).classList.add('contain__loser-bg')
+            data.displayMessages = displayMessages.loss;
+            break;
+          case gameResults.UNKNOWN:
+            (document.querySelector('.congrats') as HTMLDivElement).classList.add('congrats__error-color')
+            data.displayMessages = displayMessages.error;
+            break;
+        }
+      }, 500)
     })
-    return
+
+    return {
+      ...toRefs(data)
+    }
   }
 });
 </script>
