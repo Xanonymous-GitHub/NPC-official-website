eaf094c1a305c69255b2acadde9e242b1d58d710
Xanonymous <trusaidlin@gmail.com>
2020-12-21 15:47:46 +0800

    add new button to nav-bar and add tailwindcss config file

--- src/pages/UploadImg.vue
+++ src/pages/UploadImg.vue
@@ -1,5 +1,18 @@
 <template>
-  <div>ccccc</div>
+  <div class="">ccccc</div>
+  <button
+      class="inline-block
+      px-6 py-1 text-xs mt-2
+      font-medium leading-6
+      text-center text-white
+      uppercase transition
+      bg-green-500 rounded
+      shadow ripple
+      hover:shadow-lg hover:bg-green-600 active:bg-green-700
+      focus:outline-none"
+  >
+    Demo
+  </button>
 </template>
 
 <script lang="ts">
